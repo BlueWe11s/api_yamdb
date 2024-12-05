342b6e34918ea0ecbda31b92cdd407f07840d516
Nikita Kazantsev <kazantsev.neckit@yandex.ru>
2024-12-05 17:17:18 +0300

    -

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,63 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+from reviews.models import Category, Comment, Genre, Review, Title, Users
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Users
+        fields = ('id', 'role', 'username', 'user')
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'name', 'slug')
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ('id', 'name', 'slug')
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    genre = GenreSerializer(many=True, read_only=True)
+    category = CategorySerializer(read_only=True)
+    average_rating = serializers.FloatField(read_only=True)
+
+    class Meta:
+        model = Title
+        fields = (
+            'id', 'name', 'year', 'genre', 'description', 'category',
+            'average_rating'
+        )
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+    title = serializers.SlugRelatedField(
+        read_only=True, slug_field='name'
+    )
+
+    class Meta:
+        model = Review
+        fields = ('id', 'title', 'text', 'author', 'score', 'pub_date')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+    review = serializers.SlugRelatedField(
+        read_only=True, slug_field='id'
+    )
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'review', 'text', 'author', 'pub_date')
