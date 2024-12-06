1c9ea77233a8e3bce1c64bf7262daaf75dea046f
BlueWe11s <lshepetr@gmail.com>
2024-12-06 23:12:53 +0300

    Update urls.py

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -5,5 +5,5 @@ from .views import UserRegistrationView
 
 
 urlpatterns = [
-    path('api/v1/signup/', UserRegistrationView.as_view(), name='signup'),
+    path('v1/signup/', UserRegistrationView.as_view(), name='signup'),
 ]
